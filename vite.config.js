abbbdd4ad5c0fd99aa011a4123beaae023199eb5
Deep <deep@euclidxr.com>
2022-06-11 01:22:36 +0530

    Add github pages and vite config for base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: 'src',
+  base: '/data-viz',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
